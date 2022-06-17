d5ad295bf3827d95e2d562567d3f7691642ea995
gaurab319 <gaurab625@gmail.com>
2022-06-17 11:43:06 +0545

    Add project files.

---
+++ MVCCrudDbFirst/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿
+using MVCCrudDbFirst.Context;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace mvcDbFirstLecture.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        db_testEntities dbObj = new db_testEntities();
+        public ActionResult Student(tbl_Student obj)
+        {
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddStudent(tbl_Student model)
+        {
+            tbl_Student obj = new tbl_Student();
+            if (ModelState.IsValid)
+            {
+                obj.Id = model.Id;
+                obj.Name = model.Name;
+                obj.FName = model.FName;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+
+                if (model.Id > 0)
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.tbl_Student.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+
+
+            return RedirectToAction("StudentList");
+        }
+
+
+        public ActionResult StudentList()
+        {
+            var res = dbObj.tbl_Student.ToList();
+            return View(res);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.tbl_Student.Where(x => x.Id == id).First();
+            dbObj.tbl_Student.Remove(res);
+            dbObj.SaveChanges();
+
+            var list = dbObj.tbl_Student.ToList();
+
+            return View("StudentList", list);
+        }
+
+
+
+    }
+}
---
+++ MVCCrudDbFirst/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCCrudDbFirst")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCCrudDbFirst")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("208ab7a5-808a-4fce-b4d8-5f04da105951")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
